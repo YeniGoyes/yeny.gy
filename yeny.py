62dc595c313e39c096adfbb425efa6ad8059b73d
YeniGoyes <ygoyes@unal.edu.co>
2025-08-31 22:40:47 -0500

    Add files via upload
    Yeni Alexandra Goyes Goyes

---
+++ yeny.py
@@ -0,0 +1,41 @@
+#Resolver la suiguiénte ecuación#
+
+#Encontrar el valor de X#
+
+print("Resolver X")
+
+print("AX= B-2")
+
+#Despejar X#
+
+print("X= (B-2)/A")
+
+
+#Definir A y B#
+
+A= int(input("Ingrese el valor de A: "))
+
+print(" A=", A)
+
+B= int(input("Ingrese el valr de B: "))
+
+print("B",B)
+
+#Resolver la ecuación#
+
+X= (B-2)/A
+
+print("El valor de X es:", X)
+
+
+
+
+
+
+
+
+
+
+
+
+
